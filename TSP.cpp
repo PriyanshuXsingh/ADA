e37740e2ff0f0c0bb932c52f718a25111c4e63cb
PriyanshuXsingh <162482010+PriyanshuXsingh@users.noreply.github.com>
2025-04-15 12:00:00 +0530

    Travelling salesman

---
+++ TSP.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Tsp(vector<vector<int>>&Graph){
+    int n = Graph.size();
+for(int i=0;i<n;i++){
+    
+}
+}
+int main()
+{
+    vector<vector<int>>Graph = {
+        {0,10,15,20},
+        {5,0,9,10},
+        {6,13,0,12},
+        {8,8,9,0}
+    };
+    Tsp(Graph);
+    return 0;
+}
